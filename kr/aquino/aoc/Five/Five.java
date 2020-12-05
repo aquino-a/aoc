acd718bcf4802a5aed7c87136753bf627b95597e
alex <blackterrapin@gmail.com>
2020-12-05 15:19:07 +0900

    day five

--- kr/aquino/aoc/Five/Five.java
+++ kr/aquino/aoc/Five/Five.java
@@ -1,29 +1,41 @@
 package kr.aquino.aoc.Five;
 
-public class Five {
-    // public static void main(String[] args) throws IOException {
-    //     var arguments = new kr.aquino.aoc.Arguments(args, 2);
-    //     var path = arguments.SetText(0, "kr/aquino/aoc/Five/input.txt");
-    //     var type = arguments.SetText(1, "1").equals("1")
-    //             ? new FieldValidator[] {
-    //                  new FieldValidator("byr", null),
-    //                  new FieldValidator("iyr", null),
-    //                  new FieldValidator("eyr", null),
-    //                  new FieldValidator("hgt", null),
-    //                  new FieldValidator("hcl", null),
-    //                  new FieldValidator("ecl", null),
-    //                  new FieldValidator("pid", null)
-    //             }
-    //             : 
-    //             new FieldValidator[] {
-    //                 new FieldValidator("byr", "^(19[2-9][0-9]|200[0-2])$"),
-    //                 new FieldValidator("iyr", "^20(1[0-9]|20)$"),
-    //                 new FieldValidator("eyr", "^20(2[0-9]|30)$"),
-    //                 new FieldValidator("hgt", "^(1[5-9][0-9]cm|(59|6[0-9]|7[0-6])in)$"),
-    //                 new FieldValidator("hcl", "^#[0-9a-f]{6}$"),
-    //                 new FieldValidator("ecl", "^(amb|blu|brn|gry|grn|hzl|oth)$"),
-    //                 new FieldValidator("pid", "^\\d{9}$")
-    //            };
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.function.Function;
 
-    // }
+import kr.aquino.aoc.IOUtility;
+
+public class Five {
+    public static void main(String[] args) throws IOException {
+        var arguments = new kr.aquino.aoc.Arguments(args, 2);
+        var path = arguments.SetText(0, "kr/aquino/aoc/Five/input.txt");
+        Function<Integer[], Integer> func = arguments.SetText(1, "1").equals("1") ?
+                    (ids) -> { 
+                        return Arrays.stream(ids)
+                                .reduce((first, second) -> second)
+                                .get(); 
+                        }
+                            :
+                    (ids) -> {
+                        var count = ids[0];
+                        for (Integer id : ids) {
+                            if(!id.equals(count))
+                                return count;
+                            count++;
+                        }
+                        return -1;
+                    };
+
+
+        var seatId = func.apply(IOUtility.ReadFile(path).stream()
+                .map(s -> new BoardingPass(128, 8, s))
+                .map(bp -> bp.getSeatId()).sorted().toArray(Integer[]::new));
+                    
+
+        System.out.println(String.format("SeatId: %d", seatId));
+        
+        
+
+    }
 }
