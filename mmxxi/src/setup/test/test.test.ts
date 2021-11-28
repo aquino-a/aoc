32375afbc93444d06bac9591fdb7b2720293cb8e
alex <29205589+aquino-a@users.noreply.github.com>
2021-11-28 21:36:51 +0900

    fix read input, add fail test

--- mmxxi/src/setup/test/test.test.ts
+++ mmxxi/src/setup/test/test.test.ts
@@ -5,11 +5,17 @@ test("setup test", async () => {
     expect(setupTest()).toBe<string>('test');
 });
 
-test("input read test", async () => {
+test("input read test success", async () => {
+    const lines = await readInput(__dirname + '/testInput.txt');
+    expect(lines.length).toBe<number>(10);
+});
+
+test("input read test fail", async () => {
+    expect.assertions(1);
+
     try {
-        const lines = await readInput(__dirname + '/testInput.txt');
-        expect(lines.length).toBe<number>(10);
-    } catch (error) {
-        fail(error.message);
+        await readInput('fail');
+    } catch (e) {
+      expect(e.code).toMatch('ENOENT');
     }
 });
--- mmxxi/src/util.ts
+++ mmxxi/src/util.ts
@@ -1,15 +1,16 @@
 import { createReadStream } from "fs";
 import { createInterface } from "readline";
+import { RSA_PKCS1_OAEP_PADDING } from "constants";
 
 export const readInput = async (path: string): Promise<string[]> => {
     const lines: string[] = [];
 
     return new Promise((resolve, reject) => {
         const rs = createReadStream(path);
-        rs.on('error', reject);
+        rs.on('error', error => reject(error));
 
         createInterface({
-            input: createReadStream(path),
+            input: rs,
             terminal: false
         }).on('line', (line: string) => {
             lines.push(line);
